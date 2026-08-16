4e7d2214c83c2a54d3b075a548acf3da252ec521
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-16 15:40:34 +0300

    feat: cache composer paste guard

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v42`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v43`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
@@ -49,6 +49,7 @@
     '/composer-history.js',
     '/code-block-download.js',
     '/text-file-import.js',
+    '/composer-paste-guard.js',
     '/cloud-session-ui.js',
     '/memory-ui.js',
     '/voice-input.js',
