4cbcf51657029d0d70af64d5296143160f4e3d93
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-16 09:53:30 +0300

    feat: cache scroll-to-latest shell asset

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v22`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v23`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
@@ -30,6 +30,7 @@
     '/message-edit.js',
     '/keyboard-shortcuts.js',
     '/conversation-search.js',
+    '/scroll-to-latest.js',
     '/cloud-session-ui.js',
     '/memory-ui.js',
     '/voice-input.js',
