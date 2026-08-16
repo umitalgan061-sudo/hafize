775d42e985a51d95a839c34237d68f10a5d55bce
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-16 11:30:35 +0300

    feat: cache network status shell asset

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v30`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v31`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
@@ -38,6 +38,7 @@
     '/conversation-keyboard-nav.js',
     '/conversation-delete-confirm.js',
     '/shortcut-help.js',
+    '/network-status.js',
     '/cloud-session-ui.js',
     '/memory-ui.js',
     '/voice-input.js',
