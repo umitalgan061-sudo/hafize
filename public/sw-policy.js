f3b211a284d7fb30ba5818da35d943e175d5e9b2
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-16 13:18:16 +0300

    cache code block copy enhancement

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v34`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v35`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
@@ -42,6 +42,7 @@
     '/conversation-copy.js',
     '/in-chat-find.js',
     '/safe-markdown-render.js',
+    '/code-block-copy.js',
     '/cloud-session-ui.js',
     '/memory-ui.js',
     '/voice-input.js',
