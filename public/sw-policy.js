e522ef13f44891fb91e6057b000d9d3490a1129d
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-16 13:24:47 +0300

    chore: advance shell cache for code wrap controls

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v35`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v36`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
