b12cc5a60522652a5f8288cbb03620158cd707ac
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-18 15:24:12 +0300

    chore: refresh PWA shell for scoped schedule UI

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v85`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v86`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
