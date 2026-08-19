6fa12f7891506d4ccf75b188fa3d20b16a74ab71
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-19 20:18:03 +0300

    chore: bump PWA cache for screen share lifecycle

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v129`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v130`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
