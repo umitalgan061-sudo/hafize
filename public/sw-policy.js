0783e14db5bf1fe3ed045b6e5c3fc94406263f1d
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-19 17:21:00 +0300

    chore: bump shell cache for in-chat find lifecycle

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v125`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v126`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
