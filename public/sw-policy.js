93bba2e610111c3e1f1097eb6d1392ef18d4d265
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-19 16:25:10 +0300

    chore: refresh PWA shell for message navigation hardening

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v123`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v124`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
