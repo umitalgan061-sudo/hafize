a59313ec91ce9fe5b977bae618c77e9a8ec7af1b
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-19 14:37:40 +0300

    chore: invalidate shell cache for code focus lifecycle

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v116`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v117`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
