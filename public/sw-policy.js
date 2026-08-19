b6eead1bdf971322aea9c1279089679ec3031e6f
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-19 13:23:11 +0300

    chore: advance shell cache for composer lifecycle

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v114`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v115`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
