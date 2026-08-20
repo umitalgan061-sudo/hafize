43151cc3e79772df3efb84d5525d75a7468c529d
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-20 11:25:28 +0300

    chore: refresh shell cache for branch lifecycle

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v145`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v146`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
