b354b2f448cdb0c7241cb3643dccf8379d2bd6c2
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-20 03:24:28 +0300

    chore: refresh PWA shell for response retry lifecycle

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v135`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v136`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
