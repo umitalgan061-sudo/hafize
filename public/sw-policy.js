88b8d919a55cc20a2e6f5a6430e35fa2b8113708
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-21 12:18:09 +0300

    chore: bump PWA shell cache for search navigation

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v161`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v162`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
