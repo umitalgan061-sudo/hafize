228e056be074a57f385869c5e9b507e236c7ec51
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-21 08:21:01 +0300

    chore: bump PWA shell cache for conversation fork

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v157`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v158`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
