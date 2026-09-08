0587d13e44a00cf5645eec75d6ecf913bafee39d
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-09-08 11:36:48 +0300

    feat: cache new chat composer assets

--- public/sw-policy.js
+++ public/sw-policy.js
@@ -7,7 +7,7 @@
   'use strict';
 
   const CACHE_PREFIX = 'hafize-shell-';
-  const CURRENT_CACHE = `${CACHE_PREFIX}v15`;
+  const CURRENT_CACHE = `${CACHE_PREFIX}v16`;
   const SHELL_ASSETS = Object.freeze([
     '/',
     '/index.html',
@@ -18,7 +18,9 @@
     '/screen-share.css',
     '/hands-free.css',
     '/workspace-navigation.css',
+    '/chat-composer-features.css',
     '/app.js',
+    '/chat-composer-features.js',
     '/voice-input.js',
     '/voice-output.js',
     '/screen-share.js',
